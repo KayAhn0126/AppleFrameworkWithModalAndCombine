f70728644e298ddca85331da295144ec87b3533b
KayAhn0126 <dkseotn123@gmail.com>
2022-09-24 19:19:53 +0900

    feat: AppleFrameworkWithModalAndCombine 프로젝트 추가

---
+++ AppleFrameworkWithModalAndCombine/FrameworkCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  FrameworkCollectionViewCell.swift
+//  AppleFrameworkWithModalAndCombine
+//
+//
+
+import UIKit
+
+class FrameworkCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var thumbnailImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    func configure(_ framework: AppleFramework) {
+        thumbnailImageView.image = UIImage(named: framework.imageName)
+        nameLabel.text = framework.name
+    }
+}
